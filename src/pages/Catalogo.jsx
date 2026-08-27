50af5b6c0d2840f1457ebb4ab5f8bebd2129b0f7
Elisa Ranikelly <elisaranikelly2004@gmail.com>
2026-08-27 10:56:40 -0300

    feat: criacao tela inicial

---
+++ src/pages/Catalogo.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../componentes/Cabecalho";
+
+function Catalogo(){
+    return(
+        <div>
+            <Cabecalho />
+        </div>
+    );
+}
+
+export default Catalogo;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "../estilos/home.css";
+import Cabecalho from "../componentes/Cabecalho";
+
+function Home(){
+    return(
+        <div className="home-container">
+            
+            <Cabecalho />
+
+            <div className="fundo-container">
+
+                <div className="info-home">
+                    <h1>SEU TREINO.</h1>
+                    <h1>SEU <strong>ESTILO.</strong></h1>
+                    <h2>SUA MELHOR VERSÃO.</h2>
+                    <p>Roupas fitness de alta qualidade, para te ajudar em cada evolução.</p>
+                    
+                    <Link className="btn-entrar" to="/catalogo">ENTRAR</Link>
+                </div>
+            </div>
+
+            <div className="rodape">
+                <p>2026 - Espaço fitness. Todos os direitos reservados</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
